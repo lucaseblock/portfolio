295e3d9f0e7cdd91a1771ee37e5c560e22dc1df0
lucaseblock <lucasblock049@gmail.com>
2023-08-09 18:51:12 -0300

    gallery installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { HomeModule } from './pages/home/home.module';
+
 import { SharedModule } from './shared/shared.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeModule } from './pages/home/home.module';
 import { PortfolioModule } from './pages/portfolio/portfolio.module';
 import { ProjectsComponent } from './pages/projects/projects.component';
-//import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
+import { GALLERY_CONFIG, GalleryConfig } from 'ng-gallery';
 
 @NgModule({
   declarations: [
@@ -26,13 +28,13 @@ import { ProjectsComponent } from './pages/projects/projects.component';
     SharedModule,
   ],
   providers: [
-    // {
-    //   provide: GALLERY_CONFIG,
-    //   useValue: {
-    //     autoHeight: true,
-    //     imageSize: 'cover',
-    //   } as GalleryConfig,
-    // },
+    {
+      provide: GALLERY_CONFIG,
+      useValue: {
+        autoHeight: true,
+        imageSize: 'cover',
+      } as GalleryConfig,
+    },
   ],
   bootstrap: [AppComponent],
 })
